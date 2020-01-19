e7b38aeb801b8f1e5aaaf0e3c1c4103c27316ad3
Alex Schokking <aschokking@gmail.com>
2020-01-19 14:09:48 -0800

    Add docker-compose

--- server.py
+++ server.py
@@ -1,4 +1,10 @@
-import networktables
+import time
+from networktables import NetworkTablesInstance
 
 if __name__ == '__main__':
-    print("Hello world")
+    print("Starting NetworkTables Server")
+    nt_inst = NetworkTablesInstance.getDefault()
+    nt_inst.startServer()
+    # loop forever
+    while True:
+        time.sleep(1)
